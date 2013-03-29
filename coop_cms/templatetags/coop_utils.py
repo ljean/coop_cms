e038a26e64c75de01eb63a6e5f519d62445e900f
Luc <ljean@apidev.fr>
2013-03-29 17:49:11 +0100

    Template tag making possible to get a link to an article and to create it if it doesn't exist

---
+++ coop_cms/templatetags/coop_utils.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from coop_cms.settings import get_article_class
+from django import template
+register = template.Library()
+from django.template.defaultfilters import slugify
+
+################################################################################
+class ArticleLinkNode(template.Node):
+
+    def __init__(self, title):
+        self.title = title
+
+    def render(self, context):
+        Article = get_article_class()
+        slug = slugify(self.title)
+        try:
+            article = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            article = Article.objects.create(slug=slug, title=self.title)
+        
+        return article.get_absolute_url()
+
+@register.tag
+def article_link(parser, token):
+    title = token.split_contents()[1]
+    return ArticleLinkNode(title)
