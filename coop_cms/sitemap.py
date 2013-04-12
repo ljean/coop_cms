87aa7cd72023fe8678095b902749bfb8bb705022
Luc <ljean@apidev.fr>
2013-04-12 17:12:34 +0200

    locale sitemap

--- coop_cms/sitemap.py
+++ coop_cms/sitemap.py
@@ -3,7 +3,8 @@ from django.conf.urls.defaults import url
 from django.contrib.sitemaps import Sitemap
 from coop_cms.settings import get_article_class
 from coop_cms.models import BaseArticle
-    
+from django.conf import settings
+
 class ArticleSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.5
@@ -15,9 +16,20 @@ class ArticleSitemap(Sitemap):
     def lastmod(self, obj):
         return obj.modified
 
-sitemaps = {
-    'coop_cms': ArticleSitemap,
-}
+def get_sitemaps(langs=None):
+    if 'localeurl' in settings.INSTALLED_APPS:
+        from localeurl.sitemaps import LocaleurlSitemap
+        class LocaleArticleSitemap(LocaleurlSitemap, ArticleSitemap): pass
+        sitemaps = {}
+        lang_codes = langs or [code for (code, _x) in settings.LANGUAGES]
+        for code in lang_codes:
+            sitemaps['coop_cms_'+code] = LocaleArticleSitemap(code) 
+    else:
+        sitemaps = {
+            'coop_cms': ArticleSitemap,
+        }
+    return sitemaps
+    
 urlpatterns = (
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': get_sitemaps()}),
 )
