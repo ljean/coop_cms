0971d095244e7d0f04a19ebfa43aa66b492aaf18
Luc Jean <ljean@apidev.fr>
2014-11-14 16:23:09 +0100

    coop_bootstrap : navigation templatetag

--- coop_cms/apps/coop_bootstrap/templatetags/coop_bs.py
+++ coop_cms/apps/coop_bootstrap/templatetags/coop_bs.py
@@ -1,10 +1,16 @@
 from django import template
-register = template.Library()
 
 from coop_cms.templatetags.coop_utils import is_checkbox as _is_checkbox
+from coop_cms.templatetags.coop_navigation import NavigationAsNestedUlNode
+
+register = template.Library()
 
 #Just for compatibility
 
 @register.filter(name='is_checkbox')
 def is_checkbox(field):
   return _is_checkbox(field)
+
+@register.tag
+def navigation_bootstrap(parser, token):
+    return NavigationAsNestedUlNode(li_node="coop_bootstrap/li_node.html")
