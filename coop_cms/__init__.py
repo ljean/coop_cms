319eb80f64ecae0381d94f7886e21a737c634cab
Luc Jean <ljean@apidev.fr>
2015-02-12 09:36:51 +0100

    Fix

--- coop_cms/__init__.py
+++ coop_cms/__init__.py
@@ -2,13 +2,16 @@
 """
 coop_cms is a Content Management System for Django
 """
-from django.core.exceptions import ImproperlyConfigured
 try:
+    from django.core.exceptions import ImproperlyConfigured
     from django.conf import settings
-    if 'localeurl' in settings.INSTALLED_APPS:
-        from localeurl.models import patch_reverse
-        patch_reverse()
-except (ImportError, ImproperlyConfigured):
+    try:
+        if 'localeurl' in settings.INSTALLED_APPS:
+            from localeurl.models import patch_reverse
+            patch_reverse()
+    except ImproperlyConfigured:
+        pass
+except ImportError:
     pass
 
 VERSION = (1, 0, 10)
