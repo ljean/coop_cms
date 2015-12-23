61d11527b65ce457597604e4bcbde5c20014a45c
Luc Jean <ljean@apidev.fr>
2015-12-23 13:28:47 +0100

    Force floppy-forms 1.3.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         'django==1.6.10',  # In road to 1.9
         'south',
-        'django-floppyforms',
+        'django-floppyforms==1.3.0',
         'django-extensions',
         'sorl-thumbnail',
         'apidev-coop_colorbox >= 1.1.1',
