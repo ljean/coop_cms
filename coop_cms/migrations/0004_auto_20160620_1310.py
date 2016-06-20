c961b2bde6f5ca13888d2eeae2ba0bdaf8e494f9
Luc Jean <ljean@apidev.fr>
2016-06-20 16:20:20 +0200

    Fix homepage migration

--- coop_cms/migrations/0004_auto_20160620_1310.py
+++ coop_cms/migrations/0004_auto_20160620_1310.py
@@ -2,23 +2,24 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-
+from django.core.urlresolvers import reverse
 
 def set_homepage(apps, schema_editor):
-    # We can't import the Person model directly as it may be a newer
-    # version than this migration expects. We use the historical version.
+    # Move from homepage_for_site to SiteSettings
     site_class = apps.get_model("sites", "Site")
     for site in site_class.objects.all():
         try:
-            homepage_article = site.homepage_article
-        except models.ObjectDoesNotExist:
+            homepage_article = site.homepage_article.all()[0]
+        except (models.ObjectDoesNotExist, IndexError):
             homepage_article = None
 
         if homepage_article:
             site_settings_class = apps.get_model("coop_cms", "SiteSettings")
             site_settings = site_settings_class.objects.get_or_create(site=site)[0]
-            site_settings.homepage_url = homepage_article.get_absolute_url()
-            site_settings.save()
+            if not site_settings.homepage_url:
+                homepage_url = reverse('coop_cms_view_article', args=[homepage_article.slug])
+                site_settings.homepage_url = homepage_url
+                site_settings.save()
 
 
 class Migration(migrations.Migration):
