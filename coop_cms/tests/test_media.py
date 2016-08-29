d52cddc9d91932ef330c3d32b6ac692f90f1928d
Luc Jean <ljean@apidev.fr>
2016-08-29 11:24:48 +0200

    Refactoring use as keyword in exception handlers (Python 3)

--- coop_cms/tests/test_media.py
+++ coop_cms/tests/test_media.py
@@ -853,7 +853,7 @@ class ImageListTemplateTagTest(BaseTestCase):
         mommy.make(Image, filters=[filter_])
         try:
             Template('{% load coop_utils %}{% coop_image_list "abcd" image_list %}{{image_list|length}}')
-        except TemplateSyntaxError, msg:
+        except TemplateSyntaxError as msg:
             self.assertEqual("coop_image_list: usage --> {% coop_image_list 'filter_name' as var_name %}", unicode(msg))
         else:
             self.assertEqual("", "No exception")
