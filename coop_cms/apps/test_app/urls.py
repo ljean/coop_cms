e2cac94ab9a466c393a44e7d9d4067711b478190
Luc Jean <ljean@apidev.fr>
2017-06-09 15:23:37 +0200

    Fix warning

--- coop_cms/apps/test_app/urls.py
+++ coop_cms/apps/test_app/urls.py
@@ -9,9 +9,9 @@ from coop_cms.apps.test_app import views
 
 
 urlpatterns = [
-    url(r'^/coop-cms-testclass/$', views.TestClassListView.as_view(), name='coop_cms_testapp_list'),
-    url(r'^/works/(?P<pk>\d+)/cms_edit$', views.TestClassEditView.as_view(), name='coop_cms_testapp_edit'),
-    url(r'^/works/(?P<pk>\d+)/$', views.TestClassDetailView.as_view(), name='coop_cms_testapp_detail'),
-    url(r'^/works/cms_edit/$', views.TestClassFormsetEditView.as_view(), name='coop_cms_testapp_formset_edit'),
-    url(r'^/works/$', views.TestClassFormsetView.as_view(), name='coop_cms_testapp_formset'),
+    url(r'^coop-cms-testclass/$', views.TestClassListView.as_view(), name='coop_cms_testapp_list'),
+    url(r'^works/(?P<pk>\d+)/cms_edit$', views.TestClassEditView.as_view(), name='coop_cms_testapp_edit'),
+    url(r'^works/(?P<pk>\d+)/$', views.TestClassDetailView.as_view(), name='coop_cms_testapp_detail'),
+    url(r'^works/cms_edit/$', views.TestClassFormsetEditView.as_view(), name='coop_cms_testapp_formset_edit'),
+    url(r'^works/$', views.TestClassFormsetView.as_view(), name='coop_cms_testapp_formset'),
 ]
